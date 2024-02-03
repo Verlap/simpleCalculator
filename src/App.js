5d0a26b0dec445b418a3598f521128efde982380
Maria Mesa <maria@linknovus.com>
2024-02-03 17:05:32 -0500

    simple calculator funcionality and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState(0);
+
+  function handleButton(input, resultado) {
+    setResult(resultado);
+    input.value = "";
+    input.focus();
+  }
+
+  function resetInput() {
+    let input = document.querySelector(".numberInput");
+    input.value = "";
+    input.focus();
+  }
+  function resetResult() {
+    let input = document.querySelector(".numberInput");
+    handleButton(input, 0);
+  }
+  function add() {
+    let input = document.querySelector(".numberInput");
+    if (input.value !== "") {
+      let resultado = parseFloat(input.value) + parseFloat(result);
+      handleButton(input, resultado);
+    }
+  }
+  function minus() {
+    let input = document.querySelector(".numberInput");
+    if (input.value !== "") {
+      let resultado = parseFloat(result) - parseFloat(input.value);
+      handleButton(input, resultado);
+    }
+  }
+  function multiply() {
+    let input = document.querySelector(".numberInput");
+    if (input.value !== "") {
+      let resultado = parseFloat(input.value) * parseFloat(result);
+      handleButton(input, resultado);
+    }
+  }
+  function divide() {
+    let input = document.querySelector(".numberInput");
+    if (input.value !== "") {
+      let resultado = parseFloat(result) / parseFloat(input.value);
+      handleButton(input, resultado);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="screen">
+          <span className="result">{result}</span>
+          <input
+            className="numberInput"
+            name="numberInput"
+            type="number"
+            autoFocus
+          />
+        </div>
+        <div className="buttons">
+          <button className="reset-button" onClick={resetInput}>
+            Reset input
+          </button>
+          <button className="reset-button" onClick={resetResult}>
+            Reset result
+          </button>
+          <button className="action-button" onClick={add}>
+            +
+          </button>
+          <button className="action-button" onClick={minus}>
+            &minus;
+          </button>
+          <button className="action-button" onClick={multiply}>
+            &times;
+          </button>
+          <button className="action-button" onClick={divide}>
+            &divide;
+          </button>
+        </div>
+      </div>
+      <div className="sign">
+        <span>This project was made by Maria Mesa Rojas</span>
+      </div>
     </div>
   );
 }
